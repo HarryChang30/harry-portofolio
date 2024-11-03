cca4364555e6686615c1bf9c780735111e24858f
harrychang <harrychang120@gmail.com>
2024-11-03 12:11:16 +0700

    adding fontawesome and icon installtion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import commonjs from '@rollup/plugin-commonjs'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), commonjs()],
+	optimizeDeps: {
+		include: ['@fortawesome/fontawesome-svg-core'],
+	  },
 });
