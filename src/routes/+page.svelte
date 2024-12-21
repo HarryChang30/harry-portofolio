79e8bd0acb41cd89928d6c7b7aad59a1bd53d675
harrychang <harrychang120@gmail.com>
2024-12-21 23:55:30 +0700

    add responsive design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,38 @@
 <style global>
   @import '../app.css';
+
+  main {
+    display: flex;
+    flex-direction: column;
+    margin-left: 0;
+    padding: 8px;
+    background-color: #f7fafc;
+    min-height: 100vh;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .sidebar {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    main {
+      margin-left: 16rem; /* 64px */
+      padding: 8px;
+    }
+
+    .sidebar {
+      display: block;
+      width: 16rem; /* 64px */
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 100%;
+      background-color: #fff;
+      box-shadow: 2px 0 5px rgba(0,0,0,0.1);
+    }
+  }
 </style>
 
 
