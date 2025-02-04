2964b54a8157e82fd9427d4477fb3740613c4d8a
harrychang <harrychang120@gmail.com>
2025-02-04 20:56:26 +0700

    apply build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     commonjs(),
     {
       name: 'custom-api',
-      apply: 'serve',
+      apply: 'build',
       configureServer(server) {
         server.middlewares.use('/api/posts', async (req, res, next) => {
           const postsDir = path.resolve(__dirname, '../harry-portofolio/src/routes/posts');
